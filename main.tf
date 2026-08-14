23d9e3e8c0cad85126c3b49f73a6232fe462d997
Naresh Chintalapudi <nareshdevops1237@gmail.com>
2026-08-14 22:40:39 +0300

    Format Terraform files

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_storage_account" "serverless" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.serverless.name
   location                 = azurerm_resource_group.serverless.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
   min_tls_version = "TLS1_2"
