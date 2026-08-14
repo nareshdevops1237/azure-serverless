c1cc17669d4c904e69195051ffcfb1fd9b30e40a
Naresh Chintalapudi <nareshdevops1237@gmail.com>
2026-08-14 22:46:45 +0300

    output "storage_account_name" {   value = azurerm_storage_account.storage.name }

--- outputs.tf
+++ outputs.tf
@@ -12,13 +12,3 @@ output "function_app_default_hostname" {
   description = "Default hostname of the Azure Function App"
   value       = azurerm_linux_function_app.serverless.default_hostname
 }
-
-output "storage_account_name" {
-  description = "Name of the Azure Storage Account"
-  value       = azurerm_storage_account.serverless.name
-}
-
-output "storage_account_primary_endpoint" {
-  description = "Primary blob endpoint of the Storage Account"
-  value       = azurerm_storage_account.serverless.primary_blob_endpoint
-}
