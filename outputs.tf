1c3704f8971d6bff94678532030ff4d04be57d8f
Naresh Chintalapudi <nareshdevops1237@gmail.com>
2026-08-14 22:43:15 +0300

    made changes to the terraform.yml file to add environment: production to the terraform job. Also, made changes to main.tf to fix the account_tier attribute in the azurerm_storage_account resource.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,24 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  description = "Name of the Azure resource group"
+  value       = azurerm_resource_group.serverless.name
 }
 
 output "function_app_name" {
-  value = azurerm_linux_function_app.func.name
+  description = "Name of the Azure Function App"
+  value       = azurerm_linux_function_app.serverless.name
 }
 
 output "function_app_default_hostname" {
-  value = azurerm_linux_function_app.func.default_hostname
+  description = "Default hostname of the Azure Function App"
+  value       = azurerm_linux_function_app.serverless.default_hostname
+}
+
+output "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  value       = azurerm_storage_account.serverless.name
+}
+
+output "storage_account_primary_endpoint" {
+  description = "Primary blob endpoint of the Storage Account"
+  value       = azurerm_storage_account.serverless.primary_blob_endpoint
 }
